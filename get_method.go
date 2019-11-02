036ebc66df0bd66fd21d02b1e5d8158c241b6ef1
qwyng <ikusawasi@gmail.com>
2019-11-02 23:14:20 +0900

    add query

--- get_method.go
+++ get_method.go
@@ -4,10 +4,14 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 )
 
 func main() {
-	resp, err := http.Get("http://localhost:18888")
+	values := url.Values{
+		"query": {"hello world"},
+	}
+	resp, err := http.Get("http://localhost:18888" + "?" + values.Encode())
 	if err != nil {
 		panic(err)
 	}
