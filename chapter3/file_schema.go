b7d71191be964ab5f64b5d7c3a93637e4869f386
qwyng <ikusawasi@gmail.com>
2019-11-09 12:32:25 +0900

    mkdir chapter3

---
+++ chapter3/file_schema.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"net/http/httputil"
+)
+
+func main() {
+	transport := &http.Transport{}
+	transport.RegisterProtocol("file", http.NewFileTransport(http.Dir(".")))
+	client := http.Client{
+		Transport: transport,
+	}
+	resp, err := client.Get("file://./post.go")
+	if err != nil {
+		panic(err)
+	}
+
+	dump, err := httputil.DumpResponse(resp, true)
+	if err != nil {
+		panic(err)
+	}
+	log.Println(string(dump))
+}
